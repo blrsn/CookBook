18707d9ebcb72e2e558b39265a432a1033acb11a
larson004 <larson004@gmail.com>
2015-08-25 01:46:11 +0530

    added bootstrap and recipe page view

--- Gemfile
+++ Gemfile
@@ -3,8 +3,7 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.3'
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
@@ -23,6 +22,11 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+gem 'haml', '~> 4.0.7'
+
+gem 'simple_form', '~> 3.1.0'
+
+gem 'bootstrap-sass', '~> 3.3.5.1'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
@@ -41,5 +45,8 @@ group :development, :test do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+  
+  # Use sqlite3 as the database for Active Record
+  gem 'sqlite3'
 end
 
---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,56 @@
+class RecipesController < ApplicationController
+  before_action :get_recipe,only: [:show,:edit,:update,:destroy]
+  
+  def new
+    @recipe = Recipe.new
+  end
+  
+  def create
+    @recipe = Recipe.new(recipe_params)
+    
+    if @recipe.save
+      redirect_to @recipe 
+      flash[:success] = "Recipe saved successfully"
+    else
+      flash.now[:danger] = "Please enter valid information"
+      render 'new'
+    end
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+      flash[:notice] = "Recipe updated successfully"
+    else
+      flash.now[:danger] = "Please enter valid information"
+      render "edit"
+    end
+  end
+  
+  def destroy
+    @recipe.destroy
+    redirect_to root_path
+    flash[:success] = "Successfully deleted recipe"
+  end
+  
+  def index
+    @recipes = Recipe.all.order("created_at DESC")
+  end
+  
+  private
+  
+  def get_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+  
+  def recipe_params
+    params.require(:recipe).permit(:title,:description)
+  end
+  
+end
